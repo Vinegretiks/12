b1fa61b91ef88a5a07550172dec119c45221c5a0
Vinegretiks <146911025+Vinegretiks@users.noreply.github.com>
2023-11-25 21:12:23 +0300

    h

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+import sys
+from random import randrange
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
+from PyQt5 import uic
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        uic.loadUi('Ui.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_flag(self, qp):
+        x = randrange(1, 200)
+        y = randrange(1, 200)
+        wh = randrange(1, 200)
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(x, y, wh, wh)
+
+def excepthook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
